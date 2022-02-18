b072fd91e812d77a0952e3d67f66a0788300eb05
Nikola Prokopić <nikola@prokopic.rs>
2022-02-18 16:11:39 +0100

    Generate manifests and add crd-install

---
+++ Makefile.custom.mk
@@ -0,0 +1,23 @@
+# Generate kustomize patches and all helm charts
+.PHONY: generate
+generate:
+	./hack/generate-kustomize-patches.sh
+	$(MAKE) delete-generated-helm-charts
+	kustomize build config/helm -o helm/cluster-api-provider-aws/templates
+	./hack/move-generated-crds.sh
+	./hack/generate-crd-version-patches.sh
+
+delete-generated-helm-charts:
+	@mv ./helm/cluster-api-provider-aws/templates/ssh-sso-public-key-secret.yaml ./ssh-sso-public-key-secret.yaml
+	@rm -rf ./helm/cluster-api-provider-aws/templates/*.yaml
+	@mv ./ssh-sso-public-key-secret.yaml ./helm/cluster-api-provider-aws/templates/ssh-sso-public-key-secret.yaml
+
+CRD_BUILD_DIR := out
+
+$(CRD_BUILD_DIR):
+	mkdir -p $(CRD_BUILD_DIR)/
+
+.PHONY: release-manifests
+release-manifests: $(CRD_BUILD_DIR) ## Builds the manifests to publish with a release
+	# Build core-components.
+	kustomize build config/helm/files > $(CRD_BUILD_DIR)/crds.yaml
